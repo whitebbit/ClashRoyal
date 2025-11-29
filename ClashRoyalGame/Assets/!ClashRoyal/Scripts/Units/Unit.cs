5d353c349b5232d29be935c4dde6a780bbf37e8a
Yan Bratushchik <whitebbit.entertainment@gmail.com>
2025-11-29 13:31:19 +0300

    project

---
+++ ClashRoyalGame/Assets/!ClashRoyal/Scripts/Units/Unit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using _ClashRoyal.Scripts.Units.FSM;
+using UnityEngine;
+
+namespace _ClashRoyal.Scripts.Units
+{
+    public class Unit : MonoBehaviour
+    {
+        #region FIELDS SERIALIZED
+
+        [SerializeField] private UnitState defaultState;
+        [SerializeField] private UnitState chaseState;
+        [SerializeField] private UnitState attackState;
+
+        #endregion
+
+        #region FIELDS
+
+        private UnitState _defaultState;
+        private UnitState _chaseState;
+        private UnitState _attackState;
+
+        private UnitState _currentState;
+
+        #endregion
+
+        #region UNITY FUNCTIONS
+
+        private void Awake()
+        {
+            _defaultState = Instantiate(defaultState);
+            _chaseState = Instantiate(chaseState);
+            _attackState = Instantiate(attackState);
+
+            _currentState = defaultState;
+        }
+
+        private void Update()
+        {
+            _currentState.OnUpdate();
+        }
+
+        #endregion
+
+        #region METHODS
+
+        #endregion
+    }
+}
